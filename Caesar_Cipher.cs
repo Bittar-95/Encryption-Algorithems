c68688399109f971a2f9193c2a19270c6625c23c
Bittar-95 <47321335+Bittar-95@users.noreply.github.com>
2019-02-07 08:35:17 -0800

    Update Caesar_Cipher.cs

--- Caesar_Cipher.cs
+++ Caesar_Cipher.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Secutiry
+namespace Security
 {
     class Program
     {
@@ -15,79 +15,48 @@ namespace Secutiry
             {
                 Console.WriteLine("PLease Enter 'E' For Encryption 'D' For Decryption");
                 char type = char.Parse(Console.ReadLine().ToUpper());
+                Console.WriteLine("Please Enter The Text To Be Encrypte/Decrypte");
+                string text = Console.ReadLine().ToUpper();
+                Console.WriteLine("Plaese Enter The Key The Key Should Be Between [ 26 To -26]");
+                int key = int.Parse(Console.ReadLine());
 
-                string text;
-                int key;
+
+
                 string text_processing = "";
                 int ascii;
 
-                if (type == 'E')
+                if (type == 'D')
                 {
-                    Console.WriteLine("Please Enter The Text To Be Encrypte");
-                    text = Console.ReadLine().ToUpper();
-                    Console.WriteLine("Plaese Enter The Key The Key Should Be Between [ 26 To -26]");
-                    key = int.Parse(Console.ReadLine());
-
-                    for (int i = 0; i < text.Length; i++)
-                    {
-
-                        ascii = text[i] + key;
-                        if (key > 0 && ascii > 90)
-                        {
-                            ascii = ascii - 90;
-                            ascii = ascii + 64;
-                        }
-                        else
-                        {
-                            if (ascii < 65)
-                            {
-                                ascii = ascii - 65;
-                                ascii = ascii + 91;
-                            }
-                        }
-                        text_processing += Convert.ToChar(ascii);
-                    }
-
-                    Console.WriteLine("The Enctypted Text Is :[ {0} ]", text_processing);
-                    Console.WriteLine("---------------------------------------------------------------");
-
+                    key = key * -1;
                 }
 
-                else
+
+                for (int i = 0; i < text.Length; i++)
                 {
-                    Console.WriteLine("Please Enter Encrypted Text");
-                    text = Console.ReadLine().ToUpper();
-                    Console.WriteLine("Please enter the encryption key");
-                    key = int.Parse(Console.ReadLine());
 
-
-                    for (int i = 0; i < text.Length; i++)
-
+                    ascii = text[i] + key;
+                    if (key > 0 && ascii > 90)
                     {
-                        ascii = text[i] - key;
-                        if (key > 0 && ascii < 65)
+                        ascii = ascii - 90;
+                        ascii = ascii + 64;
+                    }
+                    else
+                    {
+                        if (ascii < 65)
                         {
                             ascii = ascii - 65;
                             ascii = ascii + 91;
-
                         }
-                        else
-                        {
-                            if (ascii > 90)
-                            {
-                                ascii = ascii - 90;
-                                ascii = ascii + 64;
-                            }
-                        }
-                        text_processing += Convert.ToChar(ascii);
                     }
-
-                    Console.WriteLine("The Decrypted Text Is :[ {0} ]", text_processing);
-                    Console.WriteLine("---------------------------------------------------------------");
-
-
-
+                    text_processing += Convert.ToChar(ascii);
                 }
+
+                Console.WriteLine("The Enctypted/Decrypted Text Is :[ {0} ]", text_processing);
+                Console.WriteLine("---------------------------------------------------------------");
+
+
+
+
                 Console.Write("Write 1 To Encryote OR Decrypte Another Text Or 0 To Exit  ");
                 loop = int.Parse(Console.ReadLine());
                 Console.WriteLine("---------------------------------------------------------------");
@@ -96,5 +65,6 @@ namespace Secutiry
 
 
         }
+        
     }
 }
